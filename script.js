b3f80494bb54e625a0a2b6c8459eb8849dca0e31
Vendum <jack59vendum@gmail.com>
2021-01-30 11:46:57 +0500

    Save

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+
+    //Основные переменные приложения.
+    var arrayAds = Array(); // Добавление новых переменных.
+
     $('#modal').hide();
 $('#add').click(function() {
     $('#modal').show();
@@ -12,5 +16,55 @@ $('#close').click(function() {
 
 });
 
+    // Добавляем новое объявление. 
+$("#new").click(function() {
+    var ad = {
+        text: $("textarea[name='text']").val(),
+        contactName: $("input[name='name']").val(), 
+        contactPhone: $("input[name='phone']").val(),
+    }
+    
+
+    arrayAds.push(ad);
+    console.log(arrayAds);
+    
+    // Отрисовка новый объявлений.
+    renderAds();
+
+    // Скрыть модальное окно.
+    $('#modal').hide();
+
+    $("textarea[name='text']").val("");
+    $("input[name='name']").val("");
+    $("input[name='phone']").val("");
+    });
+
+
+
+    // Функция отрисовки новых объявлений. 
+    function renderAds() {
+        
+
+        // Очищаем блок контента.
+        $("#content").html("");
+
+        // Отрисовываем новый блок контента.
+        arrayAds.map(ad => {
+            var adHTML = 
+            '<div class="Content__box">'+
+                    '<div class="Box__info">' + ad.text + '</div>'+
+                    '<div class="Box__name">' + ad.contactName + '</div>'+
+                    '<div class="Box__phone">' + ad.contactPhone + '</div>'+
+                '</div>';
+        
+        $("#content").append(adHTML);
+
+        });
+
+
+    }
 
 });
+
+
+
